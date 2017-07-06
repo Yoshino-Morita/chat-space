d65abd357071046b967caff93c1960c737083e39
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-06 21:12:23 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :messages
-  resources :users,only: [:delete,:create,:edit,:update]
-  resources :groups,only: [:create,:edit,:update]
+  resources :users, only: [:delete, :create, :edit, :update]
+  resources :groups, only: [:create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
