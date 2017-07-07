8f9171f40239a94a61baad3d048659495ae76e45
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-07 20:32:28 +0900

    グループ機能実装のため
    # what
    指摘箇所の削除
    
    # why
    グループ機能実装のため

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,10 +5,18 @@ class GroupsController < ApplicationController
   def update
   end
 
-  def create
-  end
-
   def new
     @group = Group.new
   end
+
+  def create
+  end
+
+
+
+  #次に使用
+  # private
+  # def group_params
+  #   params.require(:group).permit(:name)
+  # end
 end
