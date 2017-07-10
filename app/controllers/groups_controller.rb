ffb2c8e2448b5c31ed2cb7fe492b5525f7cbf30a
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-10 18:59:06 +0900

    エラーメッセージの日本語表記化
    # what
    ・グループ作成時に各テーブルへデータの保存
    ・フラッシュメッセージの実装
    
    # why
    ・グループ作成機能の実装のため

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,15 +12,15 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-       flash[:notice] = "グループを作成しました"
-       redirect_to messages_path
+       redirect_to root_path, notice: 'グループの作成が完了しました'
     else
       render :new
     end
   end
 
   private
+
   def group_params
-    params.require(:group).permit(:name, {user_ids: []})
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
