e2d3d518231badd5a8755afaf8a2e2f8173138d9
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-13 15:27:59 +0900

    メッセージ機能の実装
    # what
    ①メッセージ機能の実装
    ②画像投稿の設定
    
    # why
    
    メッセージ機能実装のため

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,24 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @message = Message.new
+    @messages = @group.messages
   end
 
   def new
-    @messages = Message.new
   end
 
   def create
+    @message = Message.create(message_params)
+   if @message.save
+      redirect_to action: :create
+   else
+    redirect_to root_path,alert: 'メッセージを入力してください'
+   end
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,14 @@
 class Group < ApplicationRecord
   has_many :members
+  has_many :messages
   has_many :users, through: :members
   validates :name, presence: true
+
+  def message_present
+    if messages.present?
+       return self.messages.last.body
+    else
+       return 'メッセージがありません'
+    end
+  end
 end
