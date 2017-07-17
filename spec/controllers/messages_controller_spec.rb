e331b2c85b289d8198869e68d41eb6d6fac96886
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-17 17:58:22 +0900

    指摘箇所の修正
    # what
    指摘箇所の修正
    # why
    コントローラのテストのため

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -6,82 +6,61 @@ describe MessagesController, type: :controller  do
      let(:message){ build(:message, user: user, group: group)}
   #indexアクション
   describe 'GET#index' do
-     context 'when user log_in' do
+     context "when user log_in" do
 
      before do
       login_user user
       get :index, params: {group_id: group}
      end
-        it"is assigns @message" do
+        it "is assigns @message" do
           expect(assigns(:message)).to be_a_new(Message)
         end
-        it"is assigns @group" do
+        it "is assigns @group" do
           expect(assigns(:group)).to eq(group)
         end
-        it"assigns the requested contact to @groups"do
-          group = user.groups.first
-          get :index, params: { group_id: group.id }
+        it "assigns the requested contact to @groups" do
           groups = user.groups
           expect(assigns(:groups)).to eq groups
         end
-        
-        #@messages
-        it"is assigns the requested contact to @messages"do
-        messages = group.messages
-        expect(assigns(:messages)).to eq messages
+        it "is assigns the requested contact to @messages" do
+          messages = group.messages
+          expect(assigns(:messages)).to eq messages
         end
-        #ビューへの遷移
-       it"renders index template?"do
-       get :index, params: {group_id: group}
-       expect(response).to render_template :index
-       end
-     end   
-   end
-  describe 'POST#create' do
+        it "renders index template?" do
+          expect(response).to render_template :index
+        end
+      end   
+    end
+  describe "POST#create" do
 
      before do
-     login_user user
+      login_user user
      end
 
-     context 'when user log_in & create success' do
+     context "when user log_in & create success" do
         #ログインしているかつ、保存に成功した場合
-         it'can create message'do
+         it "can create message" do
           expect {
             post :create, params: { message: attributes_for(:message), group_id: group.id }
           }.to change(Message, :count).by(1)
          end
 
-         it"is redirect to group_messages_path"do
+         it "is redirect to group_messages_path" do
           expect do
           post :create, params: { message: attributes_for(:message), group_id: group.id }
             expect(response).to redirect_to  group_messages_path
            end
          end
       end
-      context 'when user log_in & not create success' do
-         it'can not create message'do
+      context "when user log_in & not create success" do
+         it "can not create message" do
             expect {
             post :create, params: { message: attributes_for(:message,body: nil, image: nil), group_id: group.id }
-            }.to change(Message, :count).by(0)
+            }.not_to change(Message, :count)
          end
-        it"assigns the requested contact to @groups"do
-            group = user.groups.first
-            get :index, params: { group_id: group.id }
-            groups = user.groups
-            expect(assigns(:groups)).to eq groups
-        end
-        it"is assigns the requested contact to @messages"do
-          messages = group.messages
-          get :index, params: { group_id: group.id }
-          expect(assigns(:messages)).to eq messages
-        end
-        it"is redirect to index"do
-            post :create, params: { message: attributes_for(:message,text: nil, image: nil), group_id: group.id }
-            group = user.groups.first
-            get :index, params: { group_id: group.id }
-            groups = user.groups
-            messages = group.messages
-          expect(response).to render_template :index
+        it "is redirect to index" do
+            post :create, params: { message: attributes_for(:message, body: nil, image: nil), group_id: group.id }
+            expect(response).to render_template :index
         end
      end
   end
