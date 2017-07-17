bda1757e9e13d45d803fbe88f645417848f6141f
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-17 16:28:02 +0900

    指摘箇所の修正
    # what
    変数の確認の追加
    
    # why
    コントローラのテストのため

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -13,8 +13,7 @@ describe MessagesController, type: :controller  do
       get :index, params: {group_id: group}
      end
         it"is assigns @message" do
-        
-        expect(assigns(:message)).to be_a_new(Message)
+          expect(assigns(:message)).to be_a_new(Message)
         end
         it"is assigns @group" do
           expect(assigns(:group)).to eq(group)
@@ -44,35 +43,46 @@ describe MessagesController, type: :controller  do
      login_user user
      end
 
-     context 'when user log_in' do
+     context 'when user log_in & create success' do
         #ログインしているかつ、保存に成功した場合
-       it'can create message'do
-        expect {
-          post :create, params: { message: attributes_for(:message), group_id: group.id }
-        }.to change(Message, :count).by(1)
-       end
-
-       it"is redirect to group_messages_path"do
-        expect do
-        post :create, params: { message: attributes_for(:message), group_id: group.id }
-          expect(response).to redirect_to  group_messages_path
-         end
-       end
-         #ログインしているが、保存に失敗した場合
-       it'can not create message'do
+         it'can create message'do
           expect {
-          post :create, params: { message: attributes_for(:message,body: nil, image: nil), group_id: group.id }
-          }.to change(Message, :count).by(0)
-       end
-        it"is redirect to index"do
-          post :create, params: { message: attributes_for(:message,text: nil, image: nil), group_id: group.id }
-          groups = create_list(:group, 3)
-          get :index, params: { group_id: group.id }
-          groups = user.groups
-          messages = group.messages
-          expect(response).to render_template :index
-     end
-   end
+            post :create, params: { message: attributes_for(:message), group_id: group.id }
+          }.to change(Message, :count).by(1)
+         end
 
- end
+         it"is redirect to group_messages_path"do
+          expect do
+          post :create, params: { message: attributes_for(:message), group_id: group.id }
+            expect(response).to redirect_to  group_messages_path
+           end
+         end
+      end
+      context 'when user log_in & not create success' do
+         it'can not create message'do
+            expect {
+            post :create, params: { message: attributes_for(:message,body: nil, image: nil), group_id: group.id }
+            }.to change(Message, :count).by(0)
+         end
+        it"assigns the requested contact to @groups"do
+            group = user.groups.first
+            get :index, params: { group_id: group.id }
+            groups = user.groups
+            expect(assigns(:groups)).to eq groups
+        end
+        it"is assigns the requested contact to @messages"do
+          messages = group.messages
+          get :index, params: { group_id: group.id }
+          expect(assigns(:messages)).to eq messages
+        end
+        it"is redirect to index"do
+            post :create, params: { message: attributes_for(:message,text: nil, image: nil), group_id: group.id }
+            group = user.groups.first
+            get :index, params: { group_id: group.id }
+            groups = user.groups
+            messages = group.messages
+          expect(response).to render_template :index
+        end
+     end
+  end
 end
