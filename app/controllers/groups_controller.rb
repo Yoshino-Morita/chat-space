d34af55fef0dbae4314562fc3b1efd697a6a0871
Yoshino-Morita <yoshino@yoshinonoMacPro.local>
2017-07-19 15:30:40 +0900

    インクリメンタルサーチの実装
    # what
    インクリメンタルサーチの導入
    非同期通信化
    ビューファイルの編集
    
    # why
    インクリメンタルサーチの実装のため

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,19 +4,6 @@ class GroupsController < ApplicationController
     @groups = current_user.groups
   end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-       @group = Group.find(params[:id])
-    if @group.update(group_params)
-       redirect_to root_path, notice: 'グループの編集が完了しました'
-    else
-      render :new
-    end
-  end
-
   def new
     @group = Group.new
     @messages = Message.new
@@ -26,6 +13,19 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
        redirect_to root_path, notice: 'グループの作成が完了しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+       @group = Group.find(params[:id])
+    if @group.update(group_params)
+       redirect_to root_path, notice: 'グループの編集が完了しました'
     else
       render :new
     end
